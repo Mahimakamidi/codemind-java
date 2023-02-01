a283631dfa17ac0a68a64c1d76e99a4af60da6ad
Mahimakamidi <97445734+Mahimakamidi@users.noreply.github.com>
2023-02-01 11:44:00 +0530

    import java.util.*; class multiplication { public static void main(String args[]) {  int a,b;  Scanner sc=new Scanner(System.in);  a=sc.nextInt();  b=sc.nextInt();  int r=a*b;  System.out.println(r); } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class multiplication
+{
+public static void main(String args[])
+{
+ int a,b;
+ Scanner sc=new Scanner(System.in);
+ a=sc.nextInt();
+ b=sc.nextInt();
+ int r=a*b;
+ System.out.println(r);
+}
+}

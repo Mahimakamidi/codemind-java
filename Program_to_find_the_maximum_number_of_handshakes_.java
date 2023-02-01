5938700b379686287f3f1f923ba488c9dd330955
Mahimakamidi <97445734+Mahimakamidi@users.noreply.github.com>
2023-02-01 12:07:11 +0530

    import java.util.*; class division {     public static void main(String args[])     {         int n,a,b;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         a=n*(n-1);         b=a/2;         System.out.println(b);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class division
+{
+    public static void main(String args[])
+    {
+        int n,a,b;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        a=n*(n-1);
+        b=a/2;
+        System.out.println(b);
+    }
+}
